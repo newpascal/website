ac11daafd7729247af49882d7e98d9e7bc4be4c1
Maciej Izak <hnb.code@gmail.com>
2016-07-04 21:08:36 +0200

    Source code of webpage, initial commit.

---
+++ src/assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).foundation();
+
+function GetLatestCompilerURL() {
+  $.getJSON("https://api.github.com/repos/dathox/newpascalpack/releases/latest").done(function(json) {
+    var downloadURL = json.assets[0].browser_download_url;
+    $("#compiler-windows").attr("href", downloadURL);  
+  });    
+}
+
+$(function() {
+  GetLatestCompilerURL();
+});
