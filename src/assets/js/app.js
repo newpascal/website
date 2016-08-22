f8e49870378834d6d9253e640f56153c2cf81580
Maciej Izak <hnb.code@gmail.com>
2016-08-23 00:39:54 +0200

    mark down site section for news from github release

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,6 +4,7 @@ function GetLatestCompilerURL() {
   $.getJSON("https://api.github.com/repos/dathox/newpascalpack/releases/latest").done(function(json) {
     var downloadURL = json.assets[0].browser_download_url;
     $("#compiler-windows").attr("href", downloadURL);  
+    $("#last-news").html(markdown.toHTML(json.body)); 
   });    
 }
 
