4dac71eb6197a45d020df27b1712de15f5192b20
maciej-izak <hnb.code@gmail.com>
2016-08-23 10:48:33 +0200

    Update news/latest release section (data and news title)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,9 +2,13 @@ $(document).foundation();
 
 function GetLatestCompilerURL() {
   $.getJSON("https://api.github.com/repos/dathox/newpascalpack/releases/latest").done(function(json) {
-    var downloadURL = json.assets[0].browser_download_url;
+    var downloadURL = json.assets[0].browser_download_url,
+        md = window.markdownit(),
+        newsDate = new Date(json.published_at);
     $("#compiler-windows").attr("href", downloadURL);  
-    $("#last-news").html(markdown.toHTML(json.body)); 
+    $("#last-news-name").text(json.name); 
+    $("#last-news-date").text(newsDate.toLocaleString()); 
+    $("#last-news").html(md.render(json.body)); 
   });    
 }
 
